4fcb22f4214770040f947d548082cdcb0b07132a
yasiriqbal00777 <yasir.iqbal00777@gmail.com>
2020-06-25 01:52:42 +0500

    Updated
    Updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Made by YASIR IQBAL
         </p>
         <a
           className="App-link"
