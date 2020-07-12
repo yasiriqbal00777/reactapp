b9b34a36f74b864ca1fd8bcca57eae1d7665f8a9
yasiriqbal00777 <yasir.iqbal00777@gmail.com>
2020-07-12 17:23:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Made by <strong>YASIR IQBAL</strong> after updating Auto Deploy Script.
-        </p>
-        <div>  {2+4}  </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello World
     </div>
   );
 }
